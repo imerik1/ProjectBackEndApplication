d40006469dd559a0db147d9dbccd0050b8c10848
Erik Santana <santanaerik1106@gmail.com>
2021-03-06 15:28:32 -0300

    add method get

--- src/main/java/com/erik/backend/resources/CategoriaResource.java
+++ src/main/java/com/erik/backend/resources/CategoriaResource.java
@@ -1,4 +1,14 @@
 package com.erik.backend.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
+    @RequestMapping(method = RequestMethod.GET)
+    static String listar() {
+        return "listando";
+    }
 }
