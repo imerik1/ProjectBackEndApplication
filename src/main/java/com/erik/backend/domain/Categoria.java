2766a2349521795a28c123b995dce74f36ecfa23
Erik Santana <santanaerik1106@gmail.com>
2021-03-06 15:36:05 -0300

    create setter, getter, constructor to categoria domain

--- src/main/java/com/erik/backend/domain/Categoria.java
+++ src/main/java/com/erik/backend/domain/Categoria.java
@@ -1,4 +1,16 @@
 package com.erik.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+
 public class Categoria {
+    private Integer id;
+    private String nome;
 }
