999ef34dc10e3808d0332f8f0173945de308ebf1
Erik Santana <santanaerik1106@gmail.com>
2021-03-09 14:56:18 -0300

    service: Find by id

---
+++ src/main/java/com/erik/backend/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.erik.backend.services;
+
+import com.erik.backend.domain.Categoria;
+import com.erik.backend.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
