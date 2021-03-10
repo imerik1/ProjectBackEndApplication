ea64499ac5818fb3771c0319a157d217b3e007e1
Erik Santana <santanaerik1106@gmail.com>
2021-03-10 14:39:38 -0300

    Create service directory

--- src/main/java/com/erik/backend/resources/CategoriaResource.java
+++ src/main/java/com/erik/backend/resources/CategoriaResource.java
@@ -1,26 +1,24 @@
 package com.erik.backend.resources;
 
 import com.erik.backend.domain.Categoria;
+import com.erik.backend.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-    @RequestMapping(method = RequestMethod.GET)
-    static List<Categoria> listar() {
-        Categoria cat1 = new Categoria(1, "Informatica");
-        Categoria cat2 = new Categoria(2, "Escritorio");
+    @Autowired
+    private CategoriaService service;
 
-        List<Categoria> lista = new ArrayList<>();
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.buscar(id);
 
-        lista.add(cat1);
-        lista.add(cat2);
-
-        return lista;
+        return ResponseEntity.ok().body(obj);
     }
 }
